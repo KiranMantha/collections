d0a964fc4250c9346cc10d24a855a5435e21dd41
KiranMantha <kiranv.mantha@gmail.com>
2024-03-19 15:36:05 +0530

    added common layout and daisyui

---
+++ nextjs-ecommerce/components/ui/header/Header.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-ghost text-lg">
+            Home
+          </Link>
+          <ul>
+            <li>
+              <Link href="/cart" className="btn btn-ghost rounded-btn">
+                Cart
+              </Link>
+            </li>
+            <li>
+              <Link href="/signin" className="btn btn-ghost rounded-btn">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
