e6688be7f1863d34e658bbb29b7d050a6607b1a7
KiranMantha <kiranv.mantha@gmail.com>
2024-03-21 11:07:04 +0530

    added correlation header to all nextjs requests

--- nextjs-ecommerce/libs/services/http.service.ts
+++ nextjs-ecommerce/libs/services/http.service.ts
@@ -1,10 +1,23 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ENDPOINTS = {
   PRODUCTS: '/products'
 };
 
+const getSessionId = () => {
+  const sessionId = sessionStorage.getItem('session-id');
+  if (sessionId) return sessionId;
+  const newSessionId = uuidv4();
+  sessionStorage.setItem('session-id', newSessionId);
+  return newSessionId;
+};
+
 const makeRequest = async (endpoint: string) => {
   const url = `${process.env.NEXT_PUBLIC_REST_ENDPOINT}${endpoint}`;
-  return await fetch(url).then(res => res.json());
+  const headers = new Headers();
+  const correlationid = `${getSessionId()}_${Date.now()}`;
+  headers.append('x-correlation-id', correlationid);
+  return await fetch(url, { headers }).then(res => res.json());
 };
 
 export { ENDPOINTS, makeRequest };
