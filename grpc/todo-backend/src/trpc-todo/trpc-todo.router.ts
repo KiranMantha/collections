054cbbfcf4b177c9772d35ecf868ed6166475f24
KiranMantha <kiranv.mantha@gmail.com>
2024-06-17 09:56:59 +0530

    added tags and organised router in BE

--- grpc/todo-backend/src/trpc-todo/trpc-todo.router.ts
+++ grpc/todo-backend/src/trpc-todo/trpc-todo.router.ts
@@ -6,58 +6,68 @@ import { TrpcTodoService } from './trpc-todo.service';
 @Injectable()
 export class TrpcTodoRouter {
   private todos = [{ id: 1, text: 'Sample Todo', completed: false }];
-  constructor(private readonly trpcTodoSrvc: TrpcTodoService) {}
+  constructor(private readonly trpcTodoSrvc: TrpcTodoService) {
+    trpcTodoSrvc.mergeRouters();
+  }
 
   appRouter = this.trpcTodoSrvc.router({
-    list: this.trpcTodoSrvc.procedure
-      .meta({ openapi: { method: 'GET', path: '/todos' } })
-      .input(z.void())
-      .output(
-        z.array(
+    todo: {
+      list: this.trpcTodoSrvc.procedure
+        .meta({
+          openapi: { method: 'GET', path: '/todo/getAll', tags: ['Todo'] },
+        })
+        .input(z.void())
+        .output(
+          z.array(
+            z.object({
+              id: z.number(),
+              text: z.string(),
+              completed: z.boolean(),
+            }),
+          ),
+        )
+        .query(() => this.todos),
+      add: this.trpcTodoSrvc.procedure
+        .meta({
+          openapi: { method: 'POST', path: '/todo/add', tags: ['Todo'] },
+        })
+        .input(z.object({ text: z.string() }))
+        .output(
           z.object({
             id: z.number(),
             text: z.string(),
             completed: z.boolean(),
           }),
-        ),
-      )
-      .query(() => this.todos),
-    add: this.trpcTodoSrvc.procedure
-      .meta({ openapi: { method: 'POST', path: '/todos' } })
-      .input(z.object({ text: z.string() }))
-      .output(
-        z.object({
-          id: z.number(),
-          text: z.string(),
-          completed: z.boolean(),
+        )
+        .mutation(({ input }) => {
+          const newTodo = {
+            id: this.todos.length + 1,
+            text: input.text,
+            completed: false,
+          };
+          this.todos.push(newTodo);
+          return newTodo;
         }),
-      )
-      .mutation(({ input }) => {
-        const newTodo = {
-          id: this.todos.length + 1,
-          text: input.text,
-          completed: false,
-        };
-        this.todos.push(newTodo);
-        return newTodo;
-      }),
-    complete: this.trpcTodoSrvc.procedure
-      .meta({ openapi: { method: 'PUT', path: '/todos/{id}' } })
-      .input(z.object({ id: z.number() }))
-      .output(
-        z.object({
-          id: z.number(),
-          text: z.string(),
-          completed: z.boolean(),
+      complete: this.trpcTodoSrvc.procedure
+        .meta({
+          openapi: { method: 'PUT', path: '/todo/{id}', tags: ['Todo'] },
+        })
+        .input(z.object({ id: z.number() }))
+        .output(
+          z.object({
+            id: z.number(),
+            text: z.string(),
+            completed: z.boolean(),
+          }),
+        )
+        .mutation(({ input }) => {
+          const todo = this.todos.find((todo) => todo.id === input.id);
+          if (todo) {
+            todo.completed = true;
+          }
+          return todo;
         }),
-      )
-      .mutation(({ input }) => {
-        const todo = this.todos.find((todo) => todo.id === input.id);
-        if (todo) {
-          todo.completed = true;
-        }
-        return todo;
-      }),
+    },
   });
 
   async applyMiddleware(app: INestApplication) {
