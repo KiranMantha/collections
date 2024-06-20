e842cb4f7979fb428efbfc1ff30a0110f7f8f405
KiranMantha <kiranv.mantha@gmail.com>
2024-06-20 17:05:13 +0530

    settingup flow for types

--- react-js-security-exercise/src/components/FeedItem.js
+++ react-js-security-exercise/src/components/FeedItem.js
@@ -1,26 +1,25 @@
 // @flow
-import React from 'react';
+import React from "react";
 
 type Item = {
   feed: {
     title: string,
     description: string,
     link: string,
-  }
-}
+  },
+};
 
-
-const FeedItem = (item: Item) => {
+const FeedItem = (item: Item): React.Node => {
   return (
     <div className="row marketing">
       <div className="col-lg-12">
-          <a href={item.feed.link}>
-            <h4>{item.feed.title}</h4>
-          </a>
-          <p>{item.feed.description}</p>
+        <a href={item.feed.link}>
+          <h4>{item.feed.title}</h4>
+        </a>
+        <p>{item.feed.description}</p>
       </div>
     </div>
   );
-}
+};
 
-export default FeedItem; 
+export default FeedItem;
