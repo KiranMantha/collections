b5c029b4976ce66fdf67f8e0adbcee0df05a4e86
KiranMantha <kiranv.mantha@gmail.com>
2024-06-20 12:16:56 +0530

    updated todo app as microservice

--- grpc/todo-backend/apps/todo/src/todo.controller.ts
+++ grpc/todo-backend/apps/todo/src/todo.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Controller()
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
-  @Get()
-  getHello(): string {
-    return this.todoService.getHello();
+  @MessagePattern('createTodo')
+  create(@Payload() createTodoDto: CreateTodoDto) {
+    return this.todoService.create(createTodoDto);
+  }
+
+  @MessagePattern('findAllTodo')
+  findAll() {
+    return this.todoService.findAll();
+  }
+
+  @MessagePattern('findOneTodo')
+  findOne(@Payload() id: number) {
+    return this.todoService.findOne(id);
+  }
+
+  @MessagePattern('updateTodo')
+  update(@Payload() updateTodoDto: UpdateTodoDto) {
+    return this.todoService.update(updateTodoDto.id, updateTodoDto);
+  }
+
+  @MessagePattern('removeTodo')
+  remove(@Payload() id: number) {
+    return this.todoService.remove(id);
   }
 }
--- grpc/todo-backend/apps/todo/src/todo.module.ts
+++ grpc/todo-backend/apps/todo/src/todo.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { TodoController } from './todo.controller';
 
 @Module({
-  imports: [],
   controllers: [TodoController],
   providers: [TodoService],
 })
