c1f4dfd6de7071e9a7312ff05df1d8bca0f38acb
KiranMantha <kiranv.mantha@gmail.com>
2024-06-25 16:54:34 +0530

    code refactoring

--- grpc/server/build-protos.sh
+++ grpc/server/build-protos.sh
@@ -7,14 +7,15 @@ fi
 echo "selected proto file to compile: $1.proto"
 
 TARGET_PROTO=$1
+PROTO_GEN_PATH="./node_modules/.bin/protoc-gen-ts_proto"
+PROTO_PATH="./node_modules/protos/src"
 PROTO_DEST="./src/generated-models/${TARGET_PROTO}"
-PROTO_PATH="node_modules/protos/src"
 
 mkdir -p ${PROTO_DEST}
 
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+protoc --plugin=${PROTO_GEN_PATH} \
     -I./ \
-    --proto_path=./node_modules/protos/src \
+    --proto_path=${PROTO_PATH} \
     --ts_proto_out=${PROTO_DEST} \
     --ts_proto_opt=outputServices=grpc-js,esModuleInterop=true \
     ${TARGET_PROTO}.proto
